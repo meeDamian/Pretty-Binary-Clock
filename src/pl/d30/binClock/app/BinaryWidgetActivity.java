f5b9d1dd29f1ca1942fba1b5f374060094a6fa86
Maurycy Damian Wasilewski <damian.wasilewski@gmail.com>
2012-10-25 22:00:50 +0200

    cleanedup
    Signed-off-by: Maurycy Damian Wasilewski <damian.wasilewski@gmail.com>

--- src/pl/d30/binClock/app/BinaryWidgetActivity.java
+++ src/pl/d30/binClock/app/BinaryWidgetActivity.java
@@ -1,29 +1,36 @@
 package pl.d30.binClock.app;
 
 import static pl.d30.binClock.BinaryClockCore.PREF_NAME;
+import static pl.d30.binClock.BinaryClockCore.LOG;
 import pl.d30.binClock.R;
+import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.ImageButton;
 
 public class BinaryWidgetActivity extends Activity {
+	
+	private SharedPreferences sp;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		Log.d(LOG,"a");
 		
 		requestWindowFeature( Window.FEATURE_NO_TITLE );
 		
-		SharedPreferences sp = getSharedPreferences( PREF_NAME + "app", Context.MODE_PRIVATE );
+		sp = getSharedPreferences( PREF_NAME + "app", Context.MODE_PRIVATE );
 		
 		setContentView(R.layout.layout_app);
 		
-		if( sp.getBoolean("fullscreen", false) ) fullscreenOn(sp);
+		getWindow().clearFlags( WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON );		
+		setActive( true );
 		
 		// NOTE: enabling full screen
 		//requestWindowFeature( Window.FEATURE_NO_TITLE );
@@ -33,34 +40,29 @@ public class BinaryWidgetActivity extends Activity {
 		//getWindow().addFlags( WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON );
 		//getWindow().clearFlags( WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON );
 		
-		// NOTE: that makes system UI really low priority; should be used only after android version check
-		//getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE);
+		// NOTE: that makes system UI really low priority; should be used only after android version check		
 		
 		
-		
-		getWindow().clearFlags( WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON );
 	}
 	
-	public void toggleFullscreen(View v) {		
-		SharedPreferences sp = getSharedPreferences( PREF_NAME + "app", Context.MODE_PRIVATE );
-		if( sp.getBoolean("fullscreen", false) ) fullscreenOff( sp );
-		else fullscreenOn(sp);		
-	}	
-	private void fullscreenOn(SharedPreferences sp) {
-		((ImageButton)findViewById(R.id.fullscreen_toggle)).setImageResource(R.drawable.fullscreen_off);
-		sp.edit().putBoolean("fullscreen", true).commit();
-		getWindow().setFlags( WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN );		
+	public void toggleActive(View v) {
+		toggleActive();		
 	}
-	private void fullscreenOff(SharedPreferences sp) {
-		((ImageButton)findViewById(R.id.fullscreen_toggle)).setImageResource(R.drawable.fullscreen_on);
-		sp.edit().putBoolean("fullscreen", false).commit();
-		getWindow().clearFlags( WindowManager.LayoutParams.FLAG_FULLSCREEN );
+	private void toggleActive() {		
+		setActive( !sp.getBoolean("active", true) );	
 	}
-	
-	public void toggleStayawake(View v) {
+	@TargetApi(Build.VERSION_CODES.HONEYCOMB)
+	private void setActive(Boolean b) {
 		
-		SharedPreferences sp = getSharedPreferences( PREF_NAME + "app", Context.MODE_PRIVATE );
-		//if()
+		if( b ) {
+			sp.edit().putBoolean("active", true).commit();
+			getWindow().clearFlags( WindowManager.LayoutParams.FLAG_FULLSCREEN );
+			if( Build.VERSION.SDK_INT>=Build.VERSION_CODES.HONEYCOMB ) getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_VISIBLE);			
+		} else {
+			sp.edit().putBoolean("active", false).commit();
+			getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);			
+			if( Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB ) getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE);			
+		}
+		
 	}
-
 }
