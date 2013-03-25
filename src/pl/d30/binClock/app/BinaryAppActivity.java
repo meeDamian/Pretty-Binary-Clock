658c466afb6c74e53a63c80ae87596f89321331c
Maurycy Damian Wasilewski <damian.wasilewski@gmail.com>
2013-03-25 21:33:41 +0100

    Signed-off-by: Maurycy Damian Wasilewski <damian.wasilewski@gmail.com>

--- src/pl/d30/binClock/app/BinaryAppActivity.java
+++ src/pl/d30/binClock/app/BinaryAppActivity.java
@@ -1,16 +1,14 @@
 package pl.d30.binClock.app;
 
 import static pl.d30.binClock.BinaryClockCore.PREF_NAME;
+import static pl.d30.binClock.BinaryClockCore.APP;
+import static pl.d30.binClock.BinaryClockCore.processProperties;
 import pl.d30.binClock.R;
-import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.view.WindowManager;
 
 public class BinaryAppActivity extends Activity {
 	
@@ -24,34 +22,8 @@ public class BinaryAppActivity extends Activity {
 		newAndroid = ( Build.VERSION.SDK_INT>=Build.VERSION_CODES.HONEYCOMB ) ? true : false;				
 		sp = getSharedPreferences( PREF_NAME + "app", Context.MODE_PRIVATE );
 		
+		int[] layoutVars = processProperties( sp, APP );
+		
 		setContentView(R.layout.layout_app);
-		
-		getWindow().addFlags( WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON );			
-	}
-	protected void onStart() {
-		super.onStart();
-		setActive(true);
-	}
-	
-	public void toggleActive(View v) {
-		toggleActive();		
-	}
-	private void toggleActive() {		
-		setActive( !sp.getBoolean("active", true) );	
-	}
-
-	@TargetApi(Build.VERSION_CODES.HONEYCOMB)
-	private void setActive(Boolean active) {
-		
-		if( active ) {
-			sp.edit().putBoolean("active", true).commit();
-			getWindow().clearFlags( WindowManager.LayoutParams.FLAG_FULLSCREEN );
-			if( newAndroid ) getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_VISIBLE);			
-		} else {
-			sp.edit().putBoolean("active", false).commit();
-			getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);			
-			if( newAndroid ) getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE);			
-		}
-		
 	}
 }
